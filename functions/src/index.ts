00a01d40a6bc241dc568e61e7e757b73fc594076
C'mon Inc <43278368+comeoninc@users.noreply.github.com>
2019-07-18 11:12:17 +0700

    fire ready

---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
